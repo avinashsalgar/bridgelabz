23a44653599637c586604f45d8cfff2dea2283f3
avina <avisalgar12@gmail.com>
2022-09-22 21:31:32 +0530

    added:Display Message

---
+++ com/bridgelabz/Assigmentday1.java
@@ -0,0 +1,27 @@
+package com.bridgelabz;
+
+public class Assigmentday1 {
+    //static boolean val1;
+    //static double val2;
+    //static float val3;
+    //static int val4;
+    //static long val5;
+    //static String val6;
+    public static void main(String[] args){
+        //System.out.println("Hello class");
+        //System.out.println("Default value..");
+        //System.out.println("value1="+val1);
+        //System.out.println("value2="+val2);
+        //System.out.println("value3="+val3);
+        //System.out.println("value4="+val4);
+        //System.out.println("value5="+val5);
+        //System.out.println("value6="+val6);
+        String s1 = "AVINASH";
+        String s2 = "avinash";
+        String s3 = new String("AVINASH");
+        String s4 = "avinash";
+        System.out.println(s1.equals(s2));//true
+        System.out.println(s2.equals(s2));//true
+        System.out.println(s3.equals(s2));
+    }
+}
